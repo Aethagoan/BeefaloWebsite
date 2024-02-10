b889f02132d5cd35ddc9a13a68449f02c2a5a3da
aethagoan <aethagoan@outlook.com>
2024-02-10 14:18:09 -0700

    updated email instructions

--- ui/store.js
+++ ui/store.js
@@ -36,7 +36,13 @@ fetch("../model/items.json").then(res => res.json()).then((res) => {
 
         orderbutton.classList.add("order-button")
 
-        orderbutton.setAttribute("href", `mailto:twostatebeefalo@gmail.com?subject=Order of ${key}&body=Item(s): ${key}%0A%0AName: %0A%0APhone number: %0A%0AAddress: %0A%0A%0A`)
+        // orderbutton.setAttribute("href", `mailto:twostatebeefalo@gmail.com?subject=Order of ${key}&body=Item(s): ${key} %0A%0AName: %0A%0APhone number: %0A%0AFedex Shipping Address: %0A%0A%0A When we receive this email, you will be sent a Quickbooks invoice that will allow you to select payment methods like credit cards, paypal, or venmo. Upon completion of your payment, your order will be shipped the next Monday or Tuesday.`)
+
+        orderbutton.onclick = function() {
+            window.location.replace(`mailto:twostatebeefalo@gmail.com?subject=Order of ${key}&body=Item(s): ${key} %0A%0AName: %0A%0APhone number: %0A%0AFedex Shipping Address: %0A%0A%0A When we receive this email, you will be sent a Quickbooks invoice that will allow you to select payment methods like credit cards, paypal, or venmo. Upon completion of your payment, your order will be shipped the next Monday or Tuesday.`)
+            window.location.replace("./email.html")
+        }
+
 
         orderbutton.innerText  = `Order by Email`
 
